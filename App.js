8292d614931a56c6af1c41b2fbba4696467c1947
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-05-01 22:03:12 +0400

    Added some descriptive comments

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import HomeScreen from './screens/HomeScreen';
 import AuthLoadingScreen from "./screens/AuthLoadingScreen";
 import SignInScreen from "./screens/SignInScreen";
 
+// we'll describe all screens that are shown to authenticated user here
 const AppStack = StackNavigator({
     Home: {
         screen: HomeScreen
@@ -16,5 +17,8 @@ export default SwitchNavigator({
     App: AppStack,
     SignIn: SignInScreen
 }, {
+    // this screen runs always the first once the app is launched and decides
+    // whether to show the signin screen according to the 'userToken' key in AsyncStorage
+    // if the user is already authenticated it will take him right to the application (HomeScreen in this case)
     initialRouteName: 'AuthLoading'
 });
