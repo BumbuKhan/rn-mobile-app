fd5ff5caac4cab413a4b77b3888f370387c51d6b
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-05-13 20:08:55 +0400

    Remover useless state key

--- screens/EditProfilescreen.js
+++ screens/EditProfilescreen.js
@@ -26,8 +26,7 @@ class EditProfileScreen extends Component {
 
         },
 
-        newPasswordModalVisible: false,
-        showNewPassword: false
+        newPasswordModalVisible: false
     };
 
     setCurPasswordModalVisible(visible) {
